d477a84c5d8c4324ce000c227fe731f1cb9e31d5
Daniel Romero Bravo <126363236+DRB03@users.noreply.github.com>
2025-10-22 14:14:40 -0700

    Refactor timestamp and JSON construction in main.py
    Replaced utime.time() with utime.ticks_ms() for timestamp generation and manually constructed JSON string to avoid using the json module.

--- micropython/main.py
+++ micropython/main.py
@@ -1,6 +1,8 @@
 from microbit import *
-import utime, json
+import utime # Solo necesitamos el módulo utime
+
 DEVICE_ID = "M1"
+
 while True:
     ax = accelerometer.get_x() / 1024
     ay = accelerometer.get_y() / 1024
@@ -8,17 +10,23 @@ while True:
     tempC = temperature()
     light = display.read_light_level()
     bat = 3.1  # Voltaje simulado
-
-    data = {
-        "id": DEVICE_ID,
-        "ts": utime.time(),
-        "tempC": tempC,
-        "ax": ax,
-        "ay": ay,
-        "az": az,
-        "light": light,
-        "bat": bat
-    }
-
-    print(json.dumps(data))
+    
+    # SOLUCIÓN: Usar utime.ticks_ms() y dividir por 1000 para obtener segundos.
+    # Esto reemplaza al problemático utime.time() y genera un timestamp incremental.
+    ts = utime.ticks_ms() // 1000 
+        
+    # Construcción manual de la cadena JSON (evita el módulo "json")
+    json_string = (
+        '{"id":"' + DEVICE_ID + '",' +
+        '"ts":' + str(ts) + ',' +
+        '"tempC":' + str(tempC) + ',' +
+        '"ax":' + "{:.2f}".format(ax) + ',' + # Formateo simple para evitar demasiados decimales
+        '"ay":' + "{:.2f}".format(ay) + ',' +
+        '"az":' + "{:.2f}".format(az) + ',' +
+        '"light":' + str(light) + ',' +
+        '"bat":' + "{:.2f}".format(bat) +
+        '}'
+    )
+    
+    print(json_string)
     sleep(500)
